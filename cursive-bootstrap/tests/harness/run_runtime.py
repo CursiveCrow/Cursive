c13b7bab710caef359a8c1e3edfad27f8e8c4ad4
Crow <cursivecrow@outlook.com>
2026-01-26 17:14:08 -0800

    cleanup cont

--- cursive-bootstrap/tests/harness/run_runtime.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from run_e2e import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
--- cursive-bootstrap/tests/harness/run_semantics_oracle.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from run_eval_oracle import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
